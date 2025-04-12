ea3214c6defb83b1a05cdbc0ed5a2c921b2a9c08
Akshay Srivatsan <srivatsan@cs.stanford.edu>
2025-04-11 23:50:21 -0700

    data: continuation benchmarks

--- criu/run_benchmarks.sh
+++ criu/run_benchmarks.sh
@@ -2,7 +2,9 @@
 
 make -B
 
+echo memory, iterations, seconds
 for memory_size in 0 4096 16384 65536 262144 1048576 4194304 16777216 67108864 268435456
 do
+  sudo rm -rf /tmp/criu-test-*
   sudo ./main $memory_size 10
 done
