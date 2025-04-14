2cf5c6ca5799f563bd9046e055d71009b887d410
Akshay Srivatsan <srivatsan@cs.stanford.edu>
2025-04-13 18:46:18 -0700

    feat: adjust benchmark limits

--- criu/run_benchmarks.sh
+++ criu/run_benchmarks.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-make -B
+make -B >/dev/null
+
+KIB=1024
+MIB=$((1024 * KIB))
 
 echo memory, iterations, seconds
-for memory_size in 0 4096 16384 65536 262144 1048576 4194304 16777216 67108864 268435456
+for memory_size in 0 2 4 8 16 32 64 128 256 512 1024 2048
 do
   sudo rm -rf /tmp/criu-test-*
-  sudo ./main $memory_size 10
+  sudo ./main $(($memory_size * $MIB)) 10
 done
